b15234fb5ffb3f4d6e7bf7b31aa183df305b1708
ximonmaina <simonmaina721@gmail.com>
2019-05-22 11:49:14 +0300

    added implicit validations to the FormGroup and FormControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 import {DemoFormSkuWithBuilderComponent} from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
+import { DemoFormWithValidationsExplicitComponent } from './demo-form-with-validations-explicit/demo-form-with-validations-explicit.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoFormSkuComponent,
-    DemoFormSkuWithBuilderComponent
+    DemoFormSkuWithBuilderComponent,
+    DemoFormWithValidationsExplicitComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-demo-form-with-validations-explicit',
+  templateUrl: './demo-form-with-validations-explicit.component.html',
+  styleUrls: ['./demo-form-with-validations-explicit.component.css']
+})
+export class DemoFormWithValidationsExplicitComponent implements OnInit {
+
+  myForm: FormGroup;
+  sku: AbstractControl;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      sku: ['', Validators.required]
+    });
+
+    this.sku = this.myForm.controls.sku;
+  }
+
+  ngOnInit() {
+  }
+
+  public onSubmit(value: string): void {
+    console.log('You submitted value: ', value);
+  }
+
+}
