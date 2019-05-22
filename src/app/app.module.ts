4b3deca1c888184ab15b1b3e8e4d5af988a72e70
ximonmaina <simonmaina721@gmail.com>
2019-05-22 11:03:13 +0300

    created form component utilizing the FormBuilder module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+import {DemoFormSkuWithBuilderComponent} from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoFormSkuComponent
+    DemoFormSkuComponent,
+    DemoFormSkuWithBuilderComponent
   ],
   imports: [
     BrowserModule,
